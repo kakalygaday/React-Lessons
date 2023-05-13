3a98610903132f40a05133269281667fbc8a555a
Omar Ojarov <83600309+O-Ojarov@users.noreply.github.com>
2023-05-14 02:51:25 +0300

    Forms - Automatic focus exercise is done

---
+++ Forms-AutomaticFocus/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import UncontrolledLogin from './UncontrolledLogin';
+
+
+function App() {
+  return (
+    <div className="App">
+      <UncontrolledLogin />
+    </div>
+  );
+}
+
+export default App;
---
+++ Forms-AutomaticFocus/src/UncontrolledLogin.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef } from 'react';
+
+function UncontrolledLogin({ onLogin }) {
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const handleClickLogin = () => {
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+    onLogin({ username, password });
+  };
+  const handleResetButton = () => {
+    usernameRef.current.value = '';
+    passwordRef.current.value = '';
+  }
+  useEffect(() => {
+    usernameRef.current.focus();
+  }, []);
+
+  return (
+    <div>
+      <h1>Fill it to Login</h1>
+      <label>Username:</label>
+      <input
+        name='username'
+        ref={usernameRef}
+      />
+      <br />
+      <label>Password:</label>
+      <input
+        name='password'
+        ref={passwordRef}
+      /> <br />
+      <button onClick={handleClickLogin} >
+        Login
+      </button>
+      <button onClick={handleResetButton}>Reset</button>
+    </div>
+  );
+}
+
+export default UncontrolledLogin;
+

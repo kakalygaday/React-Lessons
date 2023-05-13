5fa792ccf7a285b52f997c9bb277f7fe9ccb3178
Omar Ojarov <83600309+O-Ojarov@users.noreply.github.com>
2023-05-13 14:56:45 +0300

    State Counter props exercise is-

---
+++ State-CounterProps/src/Counter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+export default class Counter extends React.Component {
+    state = {
+        count: this.props.initialValue
+    }
+    constructor(props) {
+        super(props)
+        setInterval(() => {
+            this.setState({
+                count: this.state.count + this.props.incrementAmount
+            })
+        }, this.props.incrementInterval);
+    }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+      </div>
+    )
+  }
+}
+

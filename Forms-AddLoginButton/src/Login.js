0b3c55096dc3ead07b27057bc32e60888344448b
Omar Ojarov <83600309+O-Ojarov@users.noreply.github.com>
2023-05-13 20:24:38 +0300

    Forms - Add login button exercise is done

---
+++ Forms-AddLoginButton/src/Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function Login({ onLogin }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleClickLogin = () => {
+    onLogin({ username, password });
+  };
+
+  const disabledButton = !username || !password;
+
+  return (
+    <div>
+      <h1>Fill it to Login</h1>
+      <label>Username:</label>
+      <input
+        name='username'
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <br />
+      <label>Password:</label>
+      <input
+        name='password'
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      /> <br />
+      <button onClick={handleClickLogin} disabled={disabledButton}>
+        Login
+      </button>
+    </div>
+  );
+}
+
+export default Login;
+

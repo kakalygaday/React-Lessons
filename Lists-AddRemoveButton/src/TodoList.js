41cde986b892414b666f50d927e0836c011a673e
Omar Ojarov <83600309+O-Ojarov@users.noreply.github.com>
2023-05-14 05:46:06 +0300

    Lists - Add a remove  exercise is done

---
+++ Lists-AddRemoveButton/src/TodoList.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+export default function TodoList() {
+    const [items, setItems] = useState([]);
+    const [newItem, setNewItem] = useState('');
+
+    const handleAddItem = () => {
+        setItems([...items, newItem]);
+        setNewItem('');
+    };
+
+    const handleReset = () => {
+        setItems([]);
+    };
+
+    const handleRemoveItem = (index) => {
+        const newItems = [...items];
+        newItems.splice(index, 1);
+        setItems(newItems);
+    };
+  return (
+    <div>
+        <ul>
+            {items.map((item, index) => (
+                <li key={index}>{item} <button onClick={() => handleRemoveItem(index)}>Remove this item</button> </li>
+            ))}
+        </ul>
+        <input type='text' placeholder='Enter new item here' value={newItem}
+        onChange={(event) => setNewItem(event.target.value)}></input>
+        <button onClick={handleAddItem}>Add Item</button> <br />
+        <button onClick={handleReset}>Reset all Item list</button>
+    </div>
+  );
+}
+

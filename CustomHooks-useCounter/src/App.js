de1aed1825db812a7db0197e59bc039782f7bf37
Omar Ojarov <83600309+O-Ojarov@users.noreply.github.com>
2023-05-18 12:25:19 +0300

    Custom Hooks - useCounter exercise is

---
+++ CustomHooks-useCounter/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CounterUse from './CounterUse';
+
+
+function App() {
+  return(
+    <div>
+      <CounterUse />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ CustomHooks-useCounter/src/CounterUse.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+const useCounter = (initialValue = 0) => {
+  const [count, setCount] = useState(initialValue);
+
+  const increment = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+
+  const reset = () => {
+    setCount(initialValue);
+  };
+
+  return { count, increment, decrement, reset };
+};
+
+const CounterUse = () => {
+  const { count, increment, decrement, reset } = useCounter(0);
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default CounterUse;
